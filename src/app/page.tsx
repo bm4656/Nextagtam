34cafa9bb2919e950c5b9a71630b6fbdb6c3ef4b
bm4656 <newjin46@gmail.com>
2023-09-21 01:14:20 +0900

    design: followingBar 스타일링

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,12 +11,12 @@ export default async function HomePage() {
 
   if (!user) return redirect('/auth/signin');
   return (
-    <section className='w-full flex flex-col md:flex-row max-w-[850px]'>
+    <section className='w-full flex flex-col md:flex-row max-w-[850px] min-w-0'>
       <div className='w-full basis-3/4'>
         <FollowingBar />
         <PostList />
       </div>
-      <div className='basis-1/4'>
+      <div className='basis-1/4 ml-8'>
         <SideBar user={user} />
       </div>
     </section>
